e05a00d669a9e3d628caa2c2692d3440d177be7f
Alex <alexscott200020@gmail.com>
2023-10-30 04:45:20 +1300

    fixed autofill

--- src/my-components/AutoComplete.js
+++ src/my-components/AutoComplete.js
@@ -21,18 +21,37 @@ const AutoComplete = ({ updateCustomerDetails }) => {
 
     const updateAddressComponents = useCallback(
         (addressComponents) => {
-            if (
-                updateCustomerDetails &&
-                Array.isArray(addressComponents) &&
-                addressComponents.length > 0
-            ) {
+            if (updateCustomerDetails && Array.isArray(addressComponents)) {
                 const updatedDetails = {
-                    streetNum: addressComponents[0]?.long_name || "",
-                    streetName: addressComponents[1]?.long_name || "",
-                    suburb: addressComponents[2]?.long_name || "",
-                    city: addressComponents[3]?.long_name || "",
-                    postCode: addressComponents[6]?.long_name || "",
+                    streetNum: "",
+                    streetName: "",
+                    suburb: "",
+                    city: "",
+                    postCode: "",
                 };
+
+                addressComponents.forEach((component) => {
+                    const componentType = component.types[0]; // Get the first type
+                    switch (componentType) {
+                        case "street_number":
+                            updatedDetails.streetNum = component.long_name;
+                            break;
+                        case "route":
+                            updatedDetails.streetName = component.long_name;
+                            break;
+                        case "locality":
+                            updatedDetails.suburb = component.long_name;
+                            break;
+                        case "administrative_area_level_2":
+                            updatedDetails.city = component.long_name;
+                            break;
+                        case "postal_code":
+                            updatedDetails.postCode = component.long_name;
+                            break;
+                        default:
+                            break;
+                    }
+                });
 
                 updateCustomerDetails(updatedDetails);
             } else {
@@ -101,7 +120,9 @@ const AutoComplete = ({ updateCustomerDetails }) => {
                     id="streetnum"
                     name="streetNum"
                     value={
-                        selectedPlace.address_components?.[0].long_name || ""
+                        selectedPlace.address_components?.find((component) =>
+                            component.types.includes("street_number")
+                        )?.long_name || ""
                     }
                     onChange={updateAddressComponents}
                     required
@@ -115,7 +136,9 @@ const AutoComplete = ({ updateCustomerDetails }) => {
                     id="streetname"
                     name="streetName"
                     value={
-                        selectedPlace.address_components?.[1].long_name || ""
+                        selectedPlace.address_components?.find((component) =>
+                            component.types.includes("route")
+                        )?.long_name || ""
                     }
                     onChange={updateAddressComponents}
                     required
@@ -129,7 +152,9 @@ const AutoComplete = ({ updateCustomerDetails }) => {
                     id="suburb"
                     name="suburb"
                     value={
-                        selectedPlace.address_components?.[2].long_name || ""
+                        selectedPlace.address_components?.find((component) =>
+                            component.types.includes("sublocality_level_1")
+                        )?.long_name || ""
                     }
                     onChange={updateAddressComponents}
                 />
@@ -142,7 +167,9 @@ const AutoComplete = ({ updateCustomerDetails }) => {
                     id="city"
                     name="city"
                     value={
-                        selectedPlace.address_components?.[3].long_name || ""
+                        selectedPlace.address_components?.find((component) =>
+                            component.types.includes("locality")
+                        )?.long_name || ""
                     }
                     onChange={updateAddressComponents}
                     required
@@ -156,7 +183,9 @@ const AutoComplete = ({ updateCustomerDetails }) => {
                     id="postcode"
                     name="postCode"
                     value={
-                        selectedPlace.address_components?.[6].long_name || ""
+                        selectedPlace.address_components?.find((component) =>
+                            component.types.includes("postal_code")
+                        )?.long_name || ""
                     }
                     onChange={updateAddressComponents}
                 />
